d204ed1f316f873a7f5c08a22463b9d09435d571
syed1RehanAhmed <syed@ampcome.com>
2023-01-02 20:11:09 +0530

    nhost

--- src/pages/posts/list.tsx
+++ src/pages/posts/list.tsx
@@ -1,146 +1,52 @@
 import {
-    IResourceComponentsProps,
-    getDefaultFilter,
+  IResourceComponentsProps,
+  getDefaultFilter,
 } from "@pankod/refine-core";
+import { useDelete, useNavigation } from "@pankod/refine-core";
 
-import {
-    List,
-    Table,
-    useTable,
-    Space,
-    EditButton,
-    ShowButton,
-    DeleteButton,
-    getDefaultSortOrder,
-    FilterDropdown,
-    Select,
-    useSelect,
-    DateField,
-    ImageField,
-} from "@pankod/refine-antd";
+import { Button, List, Table, useTable } from "@pankod/refine-antd";
 
 import { ICategory, IPost } from "interfaces";
+import { DeleteOutlined } from "@ant-design/icons";
 
-export const PostList: React.FC<IResourceComponentsProps> = () => {
-    const { tableProps, filters, sorter } = useTable<IPost>({
-        initialSorter: [
-            {
-                field: "id",
-                order: "asc",
-            },
-        ],
-        metaData: {
-            fields: [
-                "id",
-                "title",
-                {
-                    category: ["title"],
-                },
-                "content",
-                "category_id",
-                "created_at",
-                "images",
-            ],
-        },
-    });
+export const PostList: React.FC = () => {
+  const { create } = useNavigation();
+  const { mutate: deleteTodo } = useDelete();
 
-    const { selectProps } = useSelect<ICategory>({
-        resource: "categories",
-        metaData: {
-            fields: ["id", "title"],
-        },
-    });
+  const { tableProps, filters, sorter } = useTable({
+    resource: "job",
+    metaData: {
+      fields: ["id", "name"],
+    },
+  });
+  console.log(tableProps, "adasdfsdegfsrffrg");
 
-    return (
-        <List>
-            <Table {...tableProps} rowKey="id">
-                <Table.Column
-                    dataIndex="id"
-                    title="ID"
-                    sorter={{ multiple: 2 }}
-                    defaultSortOrder={getDefaultSortOrder("id", sorter)}
-                />
-                <Table.Column
-                    dataIndex="title"
-                    title="Title"
-                    sorter={{ multiple: 1 }}
-                />
-                <Table.Column<IPost>
-                    dataIndex="category_id"
-                    title="Category"
-                    filterDropdown={(props) => (
-                        <FilterDropdown {...props}>
-                            <Select
-                                style={{ minWidth: 200 }}
-                                mode="multiple"
-                                placeholder="Select Category"
-                                {...selectProps}
-                            />
-                        </FilterDropdown>
-                    )}
-                    render={(_, record) => record.category.title}
-                    defaultFilteredValue={getDefaultFilter(
-                        "category_id",
-                        filters,
-                        "in",
-                    )}
-                />
-                <Table.Column
-                    dataIndex="created_at"
-                    title="Created At"
-                    render={(value) => <DateField value={value} format="LLL" />}
-                    defaultSortOrder={getDefaultSortOrder("created_at", sorter)}
-                    sorter
-                />
-                <Table.Column
-                    dataIndex="images"
-                    align="center"
-                    title="Image"
-                    render={(value) => {
-                        return value ? (
-                            <ImageField
-                                value={value[0].url}
-                                alt={value[0]?.name}
-                                title={value[0]?.name}
-                                width={48}
-                                preview={{ mask: <></> }}
-                            />
-                        ) : (
-                            <span>---</span>
-                        );
-                    }}
-                />
-                <Table.Column<IPost>
-                    title="Actions"
-                    dataIndex="actions"
-                    render={(_, record) => (
-                        <Space>
-                            <EditButton
-                                hideText
-                                size="small"
-                                recordItemId={record.id}
-                            />
-                            <ShowButton
-                                hideText
-                                size="small"
-                                recordItemId={record.id}
-                            />
-                            <DeleteButton
-                                hideText
-                                size="small"
-                                recordItemId={record.id}
-                                metaData={{
-                                    fields: [
-                                        "id",
-                                        "content",
-                                        { category: ["id"] },
-                                    ],
-                                }}
-                            />
-                        </Space>
-                    )}
-                />
-            </Table>
-        </List>
-    );
+  return (
+    <>
+      <Button onClick={() => create("job")}> Create</Button>
+      <Table {...tableProps} rowKey="id">
+        <Table.Column title="Todos" dataIndex={"name"} />
+        <Table.Column
+          title={"Delete"}
+          render={(record: any) => (
+            <>
+              <DeleteOutlined
+                onClick={() =>
+                  deleteTodo({
+                    resource: "job",
+                    id: record?.id,
+                    successNotification: {
+                      message: "succesfully deleted Todo",
+                      description: "succesfully deleted todo",
+                      type: "success",
+                    },
+                  })
+                }
+              />
+            </>
+          )}
+        />
+      </Table>
+    </>
+  );
 };
--- src/utility/nhost.ts
+++ src/utility/nhost.ts
@@ -1,21 +1,22 @@
 import { NhostClient } from "@nhost/nhost-js";
 import { graphqlWS } from "@pankod/refine-nhost";
 
-const API_URL = "https://oxhhlmqsjahbyedrmvll.nhost.run";
-const WS_URL = "ws://oxhhlmqsjahbyedrmvll.nhost.run/v1/graphql";
+const API_URL = "https://yttmwgrgumcexfvlofxb.nhost.run";
+
+const WS_URL = "https://yttmwgrgumcexfvlofxb.nhost.run";
 
 export const nhost = new NhostClient({
-    backendUrl: API_URL,
+  backendUrl: API_URL,
 });
 
-export const gqlWebSocketClient = graphqlWS.createClient({
-    url: WS_URL,
-    connectionParams: () => {
-        const token = nhost.auth.getJWTToken();
-        return {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        };
-    },
-});
+// export const gqlWebSocketClient = graphqlWS.createClient({
+//   url: WS_URL,
+//   connectionParams: () => {
+//     const token = nhost.auth.getJWTToken();
+//     return {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     };
+//   },
+// });
